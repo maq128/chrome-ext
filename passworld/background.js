8d278dd1edab37992a6a70b2e88ee036bc282a6e
maq128 <maq128@126.com>
2014-09-10 19:53:56 +0800

    .

--- passworld/background.js
+++ passworld/background.js
@@ -1,15 +1,17 @@
 // http://developer.chrome.com/extensions/devguide.html
 
 chrome.browserAction.onClicked.addListener( function( tab ) {
+	// 查找是否已经打开了列表窗口
 	chrome.tabs.query({title:'密码保管箱'}, function(tabs) {
 		if (tabs.length > 0) {
+			// 激活该窗口
 			chrome.windows.update(tabs[0].windowId, {
 				focused: true
 			}, function(window) {
 			});
 		} else {
+			// 创建新窗口
 			chrome.windows.getCurrent({}, function(window) {
-				console.log(window);
 				var width = 300;
 				var height = 500;
 				var left = window.left + window.width - width;
--- passworld/list.js
+++ passworld/list.js
@@ -1,7 +1,30 @@
 $(function() {
 	$(window).on('resize', onResizeWindow);
 	onResizeWindow();
+
+	populateRecords(g_records);
 });
+
+var g_records = [{
+	name: '微博',
+	siteurl: 'http://weibo.com',
+	username: 'username_weibo',
+	password: 'password_weibo',
+	hint: 'hint_weibo'
+}, {
+	name: '京东',
+	siteurl: 'http://www.jd.com',
+	username: 'username_jd',
+	password: 'password_jd',
+	hint: 'hint_jd'
+}];
+
+function populateRecords(records)
+{
+	$.each(records, function(rec, idx) {
+		console.log(rec, idx);
+	});
+}
 
 function onResizeWindow()
 {
